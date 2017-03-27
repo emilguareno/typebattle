ed6d474cfd8d36bac6503d492551165b6885c703
Emil Guareno <emil.a.guareno.-nd@disney.com>
2017-03-27 10:44:27 -0700

    Cleanup of import calls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 import FullText from './FullText';
 import TextArea from './TextArea';
 import FirebaseService from './firebase';
