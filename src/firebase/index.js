d2775ae237b685e34ef4ac57bb217d2b9f99acd4
Emil Guareno <emil.a.guareno.-nd@disney.com>
2017-03-27 14:44:34 -0700

    Added config file for Firebase credentials

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,8 @@
 import * as firebase from 'firebase';
+import CONFIG from './config.json';
 class FirebaseService {
     initDatabase(){
-        firebase.initializeApp({
-            apiKey: "AIzaSyBMLx8-TBA6TH6np5JSThGDxLGzLO-nuBk",
-            authDomain: "cobattleship.firebaseapp.com",
-            databaseURL: "https://cobattleship.firebaseio.com",
-            storageBucket: "cobattleship.appspot.com",
-            messagingSenderId: "956794900976"
-        });
+        firebase.initializeApp(CONFIG);
     }
     getDatabase(){
         return firebase.database();
