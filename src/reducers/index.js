04fe5542aadecfc742e5575c521471e0f2af3994
Emil Guareno <emil.a.guareno.-nd@disney.com>
2017-04-07 17:38:42 -0700

    Added redux firebase library and completed config

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import opponent from './opponent';
-import { routerReducer } from 'react-router-redux'
+import { routerReducer } from 'react-router-redux';
+import { firebaseStateReducer } from 'react-redux-firebase';
 
 export default combineReducers({
   user,
   opponent,
-  router: routerReducer
+  router: routerReducer,
+  firebase: firebaseStateReducer
 });
--- src/store.js
+++ src/store.js
@@ -3,10 +3,16 @@ import reducers from './reducers';
 import logger from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
+import config from './firebase/config.json';
+import { reactReduxFirebase } from 'react-redux-firebase';
 
 export const history = createHistory();
 
 const routeMiddleware = routerMiddleware(history);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(reducers, composeEnhancers(applyMiddleware(logger, routeMiddleware)));
+const createStoreWithFirebase = composeEnhancers(
+    applyMiddleware(logger, routeMiddleware),
+    reactReduxFirebase(config, { userProfile: 'users' })
+)(createStore);
+export const store = createStoreWithFirebase(reducers);
