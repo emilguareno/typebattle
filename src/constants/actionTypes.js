5f15dcc6e76686f1e5698ed0a59f11010b648e12
Emil Guareno <emil.a.guareno.-nd@disney.com>
2017-04-10 15:49:26 -0700

    Changed user reducer to use action type constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,6 +3,9 @@ const actionTypes = {
   GetTextRejected: 'GET_TEXT_REJECTED',
   GetTextFulfilled: 'GET_TEXT_FULFILLED',
   OpponentIndexChanged: 'OPPONENT_INDEX_CHANGED'
+  IncrementUserWordIndex: 'INCREMENT_USER_WORD_INDEX',
+  DecrementUserWordIndex: 'DECREMENT_USER_WORD_INDEX',
+  OpponentIndexChanged: 'OPPONENT_INDEX_CHANGED',
 };
 
 export default actionTypes;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,13 +1,17 @@
+import ActionTypes from '../constants/actionTypes';
 const INITIAL_STATE = {
     wordIndex: 0
 };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'INCREMENT_USER_WORD_INDEX':
+    case ActionTypes.IncrementUserWordIndex: {
         return {
             wordIndex: state.wordIndex + 1
         };
     case 'DECREMENT_USER_WORD_INDEX':
+    }
+    case ActionTypes.DecrementUserWordIndex: {
         return {
             wordIndex: state.wordIndex - 1
         };
