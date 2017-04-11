e96e8653d9c3ce284f1878fd82c34fe838c9f0a2
Emil Guareno <eg@redlinemediagroup.com>
2017-04-10 23:20:05 -0700

    Update the wordIndex on the db after the redux state updates

--- src/actions/userWordIndexActions.js
+++ src/actions/userWordIndexActions.js
@@ -2,16 +2,27 @@ import ActionTypes from '../constants/actionTypes';
 import database from '../firebase';
 import { store } from '../store';
 
-export function incrementUserWord (dispatch){
-    const state = store.getState();
-    const userId = state.user.auth.uid;
-    database.ref(`battles/${state.battle.id}/users/${userId}`).update({wordIndex: state.user.wordIndex + 1 });
+export function incrementUserWord(dispatch) {
     dispatch({ type: ActionTypes.IncrementUserWordIndex });
 }
 
-export function decrementUserWord (dispatch){
-    const state = store.getState();
-    const userId = state.user.auth.uid;
-    database.ref(`battles/${state.battle.id}/users/${userId}`).update({wordIndex: state.user.wordIndex - 1 });
+export function decrementUserWord(dispatch) {
     dispatch({ type: ActionTypes.DecrementUserWordIndex });
 }
+
+function select(state) {
+    return state.user.wordIndex;
+}
+
+let currentValue;
+function onStateChange() {
+    let previousValue = currentValue;
+    currentValue = select(store.getState())
+    if (typeof previousValue !== 'undefined' && previousValue !== currentValue) {
+        const state = store.getState();
+        const userId = state.user.auth.uid;
+        database.ref(`battles/${state.battle.id}/users/${userId}`).update({wordIndex: currentValue });
+    }
+}
+
+store.subscribe(onStateChange);
