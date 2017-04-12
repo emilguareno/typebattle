fd04a3ae0a6b7391cca9f27dfa9ab472a3c2e329
Emil Guareno <emil.a.guareno.-nd@disney.com>
2017-04-12 16:57:03 -0700

    Connected redux index update to firebase

--- src/User/UserContainer.js
+++ src/User/UserContainer.js
@@ -1,20 +1,25 @@
 import { connect } from 'react-redux';
-import { incrementUserWord, decrementUserWord } from '../actions'
+import { getFirebase } from 'react-redux-firebase';
+import { store } from '../store';
+import { incrementUserWord, decrementUserWord } from '../actions';
 import User from './User';
 
-const mapStateToProps = (state) => {
+
+const mapStateToProps = (state, ownProps) => {
     return {
         user: state.user
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
+let props;
+const mapDispatchToProps = (dispatch, ownProps) => {
+    props = ownProps;
     return {
         onIncrementUserWord: () => {
-            incrementUserWord(dispatch);
+            dispatch(incrementUserWord());
         },
         onDecrementUserWord: () => {
-            decrementUserWord(dispatch);
+            dispatch(decrementUserWord());
         }
     }
 };
@@ -25,3 +30,23 @@ const UserContainer = connect(
 )(User);
 
 export default UserContainer;
+/**
+ * 
+ * When the user word index increases or decreases, the regular Redux state is used, as we want the 
+ * ui to be updated immediately. Below, we register to the Redux state changes and update the 
+ * firebase word index for the user whenever the state changes
+ */
+let currentValue;
+function onStateChange() {
+    let previousValue = currentValue;
+    const state = store.getState();
+    currentValue = state.user.wordIndex;
+    if (typeof previousValue !== 'undefined' && previousValue !== currentValue) {
+        const battleId = props.battle.id;
+        const { uid } = props.auth;
+        const firebase = getFirebase();
+        firebase.set(`battles/${battleId}/users/${uid}/wordIndex`, currentValue);
+    }
+}
+
+store.subscribe(onStateChange);
